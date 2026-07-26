5e113e76576f17d449833bcdd826692dd77a41a7
kr33n <lextayar@gmail.com>
2026-07-26 17:08:57 -0300

    intial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+
+import compress from "astro-compress";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://comptelinfo.com.br",
+  integrations: [sitemap()],
+  compressHTML: true,
+});
