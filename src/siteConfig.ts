87dc844446a0503db590edc488dbcf7838e1ed0b
kr33n <lextayar@gmail.com>
2026-07-26 17:07:50 -0300

    Initial commit

---
+++ src/siteConfig.ts
@@ -0,0 +1,12 @@
+export const siteConfig = {
+  name: "Comptel Informática",
+  phone: "5527996132941",
+  email: "contato@comptelinfo.com.br",
+  whatsappText: "Vim pelo site. ",
+  links: {
+    whatsappUrl: "https://wa.me/",
+    instagram: "https://ig.me/u/comptelinformatica",
+    facebook: "https://fb.me/comptelinfo",
+    title: "Fale conosco no WhatsApp",
+  },
+};
